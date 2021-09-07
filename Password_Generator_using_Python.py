1fe894a664af283e9ffae5347951c80c97b5b913
Satyamsahugr8 <72791533+Satyamsahugr8@users.noreply.github.com>
2021-09-07 08:40:35 +0530

    Add files via upload

---
+++ Password_Generator_using_Python.py
@@ -0,0 +1,20 @@
+# i have used string module to take input alphabet 
+import string
+import random
+
+if __name__ == "__main__":
+    s1 = string.ascii_lowercase
+    s2 = string.ascii_uppercase
+    s3 = string.digits
+    s4 = string.punctuation
+    pass_len = int(input("Enter password length\n")) #Todo1: Handle Gibberish
+    s = []
+    s.extend(list(s1))
+    s.extend(list(s2))
+    s.extend(list(s3))
+    s.extend(list(s4))
+    random.shuffle(s)
+    print("Your password is: ")
+    print("".join(s[0:pass_len]))
+    # print("".join(s[0:plen]))
+
